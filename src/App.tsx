06b23de4e062a1c15bbcd766b894364486b29446
claudiateres <claudia.teres@koahealth.com>
2022-06-28 20:22:36 +0200

    Stage1: add tests for UI & display interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Add, Subtract } from './functionalities';
 
-const data = [
+interface ICounter {
+  id: number;
+  value: number;
+}
+
+interface IProps {
+  counterId: number;
+  value: number;
+  // eslint-disable-next-line no-unused-vars
+  handleChange: (counterId: number, newValue: number) => void;
+}
+
+const data: ICounter[] = [
   { id: 1, value: 0 },
   { id: 2, value: 0 },
   { id: 3, value: 0 },
 ];
 
-function Counter(props) {
-  const { value } = props;
+function Counter({ counterId, value, handleChange }: IProps) {
   return (
     <div>
-      <b>{value}</b>
+      <b data-testid={`counter-${counterId}`}>{value}</b>
       <div>
-        <button>-</button>
-        <button>+</button>
+        <button data-testid={`subtraction-btn-${counterId}`} type="button" onClick={() => handleChange(counterId, Subtract(value))}>-</button>
+        <button data-testid={`addition-btn-${counterId}`} type="button" onClick={() => handleChange(counterId, Add(value))}>+</button>
       </div>
     </div>
   );
 }
 
 function App() {
+  const [counters, setCounters] = useState(data);
+
+  const handleChange = (counterId: number, newValue: number): void => {
+    const newCounters = counters.map((counter) => {
+      if (counter.id === counterId) {
+        return { ...counter, value: newValue };
+      }
+      return counter;
+    });
+
+    setCounters(newCounters);
+  };
+
   return (
     <div>
-      {data.map((counter) => (
-        <Counter key={counter.id} value={counter.value} />
+      {counters.map((counter) => (
+        <Counter
+          key={counter.id}
+          counterId={counter.id}
+          value={counter.value}
+          handleChange={handleChange}
+        />
       ))}
     </div>
   );
--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from '../App';
 
-test('should show 3 initial 0s', () => {
+test('Counter should show 3 initial 0s', () => {
   const { getAllByText } = render(<App />);
   const linkElement = getAllByText('0');
   expect(linkElement.length).toEqual(3);
+});
+
+test('Counter 1 should show an increment of 1 when pressing +', async () => {
+  render(<App />);
+
+  const counter = screen.getByTestId('counter-1');
+  const incrementBtn = screen.getByTestId('addition-btn-1');
+
+  fireEvent.click(incrementBtn);
+
+  expect(counter).toHaveTextContent('1');
+});
+
+test('Counter 2 should show an decrement of 1 when pressing -', async () => {
+  render(<App />);
+
+  const counter = screen.getByTestId('counter-2');
+  const decrementBtn = screen.getByTestId('subtraction-btn-2');
+
+  fireEvent.click(decrementBtn);
+
+  expect(counter).toHaveTextContent('-1');
+});
+
+test('Counter 3 should show initial value when pressing - and +', async () => {
+  render(<App />);
+
+  const counter = screen.getByTestId('counter-3');
+  const decrementBtn = screen.getByTestId('subtraction-btn-3');
+  const incrementBtn = screen.getByTestId('addition-btn-3');
+
+  fireEvent.click(decrementBtn);
+  fireEvent.click(incrementBtn);
+
+  expect(counter).toHaveTextContent('0');
 });
