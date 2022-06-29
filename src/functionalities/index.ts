78e62b6756887aa55242bcd0af602165c8d35e85
claudiateres <claudia.teres@koahealth.com>
2022-06-29 13:28:37 +0200

    Stage2: refactor Add functionality

--- src/functionalities/index.ts
+++ src/functionalities/index.ts
@@ -2,9 +2,9 @@ export function Increment(initialValue: number): number { return initialValue + 
 
 export function Decrement(initialValue: number): number { return initialValue - 1; }
 
-export function Add(initialValues: number[]): number {
+export function Add(initialValues: number[], { initialValue = 0 }): number {
   return initialValues.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
-    0,
+    initialValue,
   );
 }
